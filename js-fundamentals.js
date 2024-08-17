f8b76650b588dece0989a000f736f7bce6dbf9fa
Maaxboon <maaxwelokoth@gmail.com>
2024-08-17 19:54:16 +0300

    High level overview of variables

--- js-fundamentals.js
+++ js-fundamentals.js
@@ -1,2 +1,24 @@
 // output "Hello, world!"
-console.log("Hello, world!");
+// console.log("Hello, world!");
+
+// variables: an element, feature or factor that is liable to change or vary.
+
+// let: declares a variable
+// var
+
+// let firstName = 'Maax';
+
+// naming variables
+
+// Camel Case -> firstNameOfMaax
+
+// Pascal Case -> FirstNameOfMaax
+
+// snake case -> first_name_of_maax
+
+//  constant: remaining the same over a period of time.
+const firstName = 'Maax';
+
+// firstName = 'Max';
+
+console.log('{firstName}');
