df0f8f08be485807d58181b78362f1fb3b8ca1d3
Maaxboon <maaxwelokoth@gmail.com>
2024-08-18 17:21:45 +0300

    Adds data types

--- js-fundamentals.js
+++ js-fundamentals.js
@@ -1,24 +1,6 @@
-// output "Hello, world!"
-// console.log("Hello, world!");
-
-// variables: an element, feature or factor that is liable to change or vary.
-
-// let: declares a variable
-// var
-
-// let firstName = 'Maax';
-
-// naming variables
-
-// Camel Case -> firstNameOfMaax
-
-// Pascal Case -> FirstNameOfMaax
-
-// snake case -> first_name_of_maax
-
-//  constant: remaining the same over a period of time.
 const firstName = 'Maax';
+const lastName = 'Boon';
 
-// firstName = 'Max';
+const combined = firstName + " " + lastName;
 
-console.log('{firstName}');
+console.log(combined);
