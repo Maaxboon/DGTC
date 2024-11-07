793ef05bdc6d9d05bc0df6c3e331466360d72473
Maaxboon <maaxwelokoth@gmail.com>
2024-11-07 14:27:49 +0300

    Updates modulo

--- numbers.js
+++ numbers.js
@@ -28,3 +28,38 @@ console.log(24 % 5); //4r4
 console.log(25 % 5); //5r0
 console.log(26 % 5); //5r1
 console.log(7 % 10); //0r7
+console.log(4 / 2); // 0
+console.log(4 % 2); // 2r0
+
+// 4 / 2 = 2
+console.log(4 / 2);
+//With a remainder of 0
+console.log(4 % 2);
+
+// 7 / 2 = 3.5
+console.log(7 / 2);
+//With a remainder of 1
+console.log(7 % 2);
+
+// If a number modulus other number is equal to 0
+// it is a multiple of "other number"
+
+// 8 is indeed a multiple of 2!
+console.log(8 % 2 === 0);
+// 9 is NOT a multiple of 2!
+console.log(9 % 2 === 0);
+
+// Numbers
+// Integers
+const age = 34;
+
+// Floating point numbers
+const price = 12.99;
+
+// NaN
+
+const name = "Messi";
+
+const howManyGoals = name / 2;
+
+console.log(howManyGoals); // NaN: Not a Number
