8eb7b015fa9daec3d255ef5f8862eb77149201f9
Maaxboon <maaxwelokoth@gmail.com>
2024-11-14 17:33:01 +0300

    Adds variables.js

---
+++ JavaScript_Basics/variables.js
@@ -0,0 +1,9 @@
+let playerName = "Michael Olunga"; // String
+const age = 30; // Number
+var isProfessional = true; // Boolean
+let teamsPlayed = ["Gor Mahia", "Girona", "Kashiwa Reysol", "Harambee Stars"]; // Array
+
+console.log("Player:", playerName);
+console.log("Age:", age);
+console.log("Is Professional:", isProfessional);
+console.log("Teams played: ", teamsPlayed);
