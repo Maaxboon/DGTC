2b9d3a6143448e60fde8f748fb450b309012c4e6
Maaxboon <maaxwelokoth@gmail.com>
2024-11-14 18:04:44 +0300

    Adds time taken to CBD

---
+++ JavaScript_Basics/functions.js
@@ -0,0 +1,11 @@
+function calculateSuccessRate(goals, attempts) {
+  return (goals / attempts) * 100;
+}
+let successRate = calculateSuccessRate(10, 15);
+console.log("Success Rate:", successRate + "%");
+
+function timeToReachCBD(distance, speed) {
+  return distance / speed / 60;
+}
+let timeTaken = timeToReachCBD(26000, 3600);
+console.log("Time taken to reach CBD:", timeTaken + "hours");
