f9240542fdd4039a40c8c235281531dbf04acc0b
Maaxboon <maaxwelokoth@gmail.com>
2024-11-15 11:03:15 +0300

    Adds greeting

---
+++ greeting.js
@@ -0,0 +1,8 @@
+// Store the users name in a variable
+let userName = 'Marcin';
+
+// Create a message using the name
+let greeting = "Hello, " + userName + "!";
+
+// Display greeting on the screen
+console.log(greeting);
