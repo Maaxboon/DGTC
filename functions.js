a9b77ee8ef9a0fe5eb79c10fa0adfe66e197c7a9
Maaxboon <maaxwelokoth@gmail.com>
2024-11-20 20:56:57 +0300

    Adds functions.js

---
+++ functions.js
@@ -0,0 +1,10 @@
+// A function is a procedure of code that will run when called
+// When written we can use it as many times as we please.
+// We only write a function once
+// Writing a function is known as function declaration or function definition
+
+//  The example below declares a function named average and invokes it later on.
+function average(number1, number2) {
+  return (number1 + number1) / 2;
+}
+average();
