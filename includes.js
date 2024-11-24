b151c3525d4dcee5ee38df0f3c6242cf6261ab57
Maaxboon <maaxwelokoth@gmail.com>
2024-11-24 13:34:24 +0300

    Using includes to determine if a given value exists in an array

---
+++ includes.js
@@ -0,0 +1,10 @@
+const array1 = [1, 2, 3];
+
+console.log(array1.includes(2)); // Expected output: true
+
+const pets = ['cat', 'dog', 'hamster'];
+
+console.log(pets.includes('hamster'));
+// Expected output: true
+console.log(pets.includes('at'));
+// Expected output: false
