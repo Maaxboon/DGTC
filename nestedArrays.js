5e8a3558a0e28115ae7aa62fb8ac7493b567c860
Maaxboon <maaxwelokoth@gmail.com>
2024-11-24 16:21:37 +0300

    Nested arrays in arrays

---
+++ nestedArrays.js
@@ -0,0 +1,7 @@
+const starter = ['tomato', 'cream', 'basil'];
+const main = ['lemon', 'thyme', 'chicken'];
+const dessert = ['apple', 'pastry', 'cream'];
+
+const dinner = [starter, main, dessert];
+
+console.log(dinner);
