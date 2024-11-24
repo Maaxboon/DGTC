6eecfee22780dde578a369ed868916ea41cc1f2e
Maaxboon <maaxwelokoth@gmail.com>
2024-11-24 13:33:35 +0300

    Slicing arrays

---
+++ slice.js
@@ -0,0 +1,9 @@
+const shoppingList = ['brocolli', 'chocolate', 'mushrooms', 'baked beans', 'butter', 'tomatoes'];
+
+const firstThreeItems = shoppingList.slice(0, 3);
+
+console.log(firstThreeItems);
+
+const lastThreeItems = shoppingList.slice(-3);
+
+console.log(lastThreeItems);
