a3e2934b26ee9103acca4a45280e89f5d9f250f8
Maaxboon <maaxwelokoth@gmail.com>
2024-12-09 08:43:28 +0300

    Adds chained if else loop

---
+++ JavaScript_Basics/chained_if_else.js
@@ -0,0 +1,9 @@
+const northcoder = 'Maaxboon';
+
+if (northcoder === 'Ali') {
+	console.log("Hello Ali");
+} else if (northcoder === 'Kyle') {
+	console.log("Hello Kyle!");
+} else {
+	console.log("I don't know your name!");
+};
