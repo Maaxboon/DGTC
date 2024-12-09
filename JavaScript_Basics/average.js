ad1fd3019eac6b83fbc590c93737da0f8fa6c30e
Maaxboon <maaxwelokoth@gmail.com>
2024-12-09 08:47:19 +0300

    Finding average using a function

---
+++ JavaScript_Basics/average.js
@@ -0,0 +1,7 @@
+// Function definition
+function average(number1, number2) {
+  return (number1 + number1) / 2;
+}
+
+// This function call passes the arguments 10 and 16
+console.log(average(10, 16));
