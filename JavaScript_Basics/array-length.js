c1008ad565fbd0b9a96247c407d8f40109c8908e
Maaxboon <maaxwelokoth@gmail.com>
2025-01-09 23:00:10 +0300

    Checking the length of an array

---
+++ JavaScript_Basics/array-length.js
@@ -0,0 +1,7 @@
+const trainStations = ['Manchester Piccadilly', 'Huddersfield', 'Leeds', 'Hull'];
+
+console.log(trainStations.length);
+
+const lastStop = trainStations[trainStations.length -1];
+
+console.log(lastStop);
