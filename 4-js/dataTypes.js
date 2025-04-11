d9100880440d38c021ffc7517c9b697b17acc727
Maxwel Okoth <maaxwelokoth@gmail.com>
2025-04-11 07:16:31 +0300

    Defining Strings

--- 4-js/dataTypes.js
+++ 4-js/dataTypes.js
@@ -9,3 +9,9 @@ const number1 = 5; // definition of an integer
 const number2 = 0.5; // definition of a decimal
 const number3 = -22; // definition of a negative number
 const number4 = -0.9; // definition of a negative decimal
+
+// Strings
+const firstName = "Wuod"; // Double quotes
+const lastName = "Jadolo"; // Double quotes
+const age = "18"; // not a number but a string
+// const street = 'Bumbogo Cell"; // Syntax error: mixed form
