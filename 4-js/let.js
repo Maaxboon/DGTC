1a9b52c4d744f5e85392c225433ef74c164cd2cb
Maxwel Okoth <maaxwelokoth@gmail.com>
2025-04-11 06:56:32 +0300

    Declaring Variables Using the "let" Keyword

---
+++ 4-js/let.js
@@ -0,0 +1,7 @@
+let firstName; // variable declaration
+firstName = "Maax"; // variable initialization
+
+let lastName = "Boon"; // combined variable declaration and variable initialization
+
+console.log(firstName);
+console.log(lastName);
