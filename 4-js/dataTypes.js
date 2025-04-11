985ab1be6da8cb8b5fba2649aae891dd4a60c743
Maxwel Okoth <maaxwelokoth@gmail.com>
2025-04-11 07:12:04 +0300

    Defining Different Number Variables

---
+++ 4-js/dataTypes.js
@@ -0,0 +1,11 @@
+// JavaScript provides a total of six (or strictly speaking seven, see box) different data types
+
+/*primitive data types:
+ for representations of numbers (data type number), character strings (data type string), and Boolean truth values (data type boolean).
+ Then, we have the special data types null and undefined (also a form of primitive data types) as l as a further data type for objects*/
+
+//  Numbers
+const number1 = 5; // definition of an integer
+const number2 = 0.5; // definition of a decimal
+const number3 = -22; // definition of a negative number
+const number4 = -0.9; // definition of a negative decimal
