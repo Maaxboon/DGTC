67f27701dee38e5fb741eb115fc20095b1ea4f75
Maxwel Okoth <maaxwelokoth@gmail.com>
2025-04-11 07:38:23 +0300

    JavaScript operators

---
+++ 4-js/operators.js
@@ -0,0 +1,42 @@
+// Arithmetic operators
+let first = 8;
+
+let second = 2;
+
+let sum = first + second; // Addition
+
+let difference = first - second; // subtraction
+
+let product = first * second; // multiplication
+
+let division = first / second; // division
+
+let remainder = first % second; // modulo
+
+/*
+++ : increment operator
+--: decrement operator
+**: calculate powers
+=: assignement operator
++: concatenation operator
+*/
+
+/*Logical operators
+&&: AND operator, checks whether two boolean values are true
+||: OR operator, checks whether at least one of two boolean values is true
+!: Negation operator, reverses a Boolean truth value, that is . turns true into flase and vice versa
+*/
+
+// Bitwise operators
+
+/*
+Comparison operators
+< less than
+> greater thans
+<= less than or equal to
+>= greater than or equal to
+== equality
+=== strict equality
+!= inequality
+!== strict inequality
+*/
