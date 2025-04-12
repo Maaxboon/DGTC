695e2802f5fe120b6ba78efc82bb858390555dd9
Maxwel Okoth <maaxwelokoth@gmail.com>
2025-04-12 13:42:55 +0300

    Using function parameters

---
+++ 4-js/parameters.js
@@ -0,0 +1,16 @@
+/*const printNumbersFromXToY = function (x, y) {
+  for (let i = x; i <= y; i++) {
+    console.log(i);
+  }
+};
+printNumbersFrom1To10();*/
+
+function printNumbersFromXToY(x, y) {
+  for (let i = x; i <= y; i++) {
+    console.log(i);
+  }
+}
+printNumbersFromXToY(6, 13); // numbers 6 to 13
+printNumbersFromXToY(1, 10); // numbers 1 to 10
+printNumbersFromXToY(1, 100); // numbers 1 to 100
+printNumbersFromXToY(100, 1000); // numbers 100 to 1000
