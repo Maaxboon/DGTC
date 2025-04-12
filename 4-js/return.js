7a3ebd1ecd924fd4c5a790d6c06ff563db2912d6
Maxwel Okoth <maaxwelokoth@gmail.com>
2025-04-12 14:00:01 +0300

    Functions That Return Values

---
+++ 4-js/return.js
@@ -0,0 +1,31 @@
+// Addition
+function sum(x, y) {
+  let result = x + y; // Addition of the two passed parameters
+  return result; // Return of the result
+}
+const z = sum(5, 6); // Call and assignment of the return value
+console.log(z);
+
+// Multiplication
+function product(a, b) {
+  let output = a * b;
+  return output;
+}
+const p = product(5, 6);
+console.log(p);
+
+// Subtraction
+function difference(d, r) {
+  let diff = d - r;
+  return diff;
+}
+const m = difference(9, 4);
+console.log(m);
+
+// Division
+function division(f, u) {
+  let dive = f / u;
+  return dive;
+}
+const w = division(9, 3);
+console.log(w);
