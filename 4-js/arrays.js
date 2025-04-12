9eb36c1c67c2c0a9cc3c0e06f74a66ca0b50db1d
Maxwel Okoth <maaxwelokoth@gmail.com>
2025-04-12 16:44:16 +0300

    Creating an Array Using Short Notation

---
+++ 4-js/arrays.js
@@ -0,0 +1,27 @@
+// Creating an array with a specified length
+const names = ["Maaxboon", "Uri-X", "Poisedon", "Jok-Maker"];
+
+// Accessing the elements of an array
+console.log(names[0]); // Maaxboon
+console.log(names[1]); // Uri-X
+console.log(names[2]); // Poisedon
+console.log(names[3]); // Jok-Maker
+
+// Creating an empty array
+const colors = [];
+console.log(colors);
+
+// Adding values
+colors.push("red");
+console.log(colors);
+colors.push("blue");
+console.log(colors);
+
+// Adding to the beginning of an array
+colors.unshift("green");
+console.log(colors);
+
+// Adding values via index notation
+colors[3] = "orange";
+colors[4] = "yellow";
+console.log(colors);
