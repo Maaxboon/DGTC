d1115f1e939e86b3b2c2381c5b5ef6b645ec9c2b
Maxwel Okoth <maaxwelokoth@gmail.com>
2025-04-15 15:51:19 +0300

    JSON Embedded Directly within JavaScript Code

---
+++ 6-using-web-formats/jsonEmbed.js
@@ -0,0 +1,25 @@
+const person = {
+  firstname: "John",
+  lastname: "Doe",
+  phone: {
+    type: "cell",
+    number: "123456789",
+  },
+  email: "john.doe@example.com",
+  address: {
+    street: "Sample Street",
+    number: 99,
+    code: "12345",
+    city: "Sample City",
+  },
+};
+
+console.log(person.firstname); // "John"
+console.log(person.lastname); // "Doe"
+console.log(person.phone.type); // "cell"
+console.log(person.phone.number); // "01234567"
+console.log(person.email); // "john.doe@example.com"
+console.log(person.address.street); // "Sample Street"
+console.log(person.address.number); // 99
+console.log(person.address.code); // "12345"
+console.log(person.address.city); // "Sample City"
