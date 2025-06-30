2e3f894f3431c801dd6575f7f1ebaffc5f7e3b78
Maaxboon <maaxwelokoth@gmail.com>
2025-06-30 19:34:19 +0300

    Switch statements

---
+++ js_tutorials/js7/js/switch.js
@@ -0,0 +1,19 @@
+// Conditional Statements: Switch statements
+
+const { Children } = require("react");
+
+// Syntax
+switch (expression OR value) {
+    case choice1:
+        // run this code
+        break;
+    case choice2:
+        // run this code
+        break;
+    
+    // add as many cases as needed
+
+    default:
+        // run this code if no case matches
+        // no need for break here
+}
