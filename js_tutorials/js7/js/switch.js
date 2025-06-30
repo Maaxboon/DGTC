c675742b9182759f52a7130a42a8b37e7224b405
Maaxboon <maaxwelokoth@gmail.com>
2025-06-30 19:41:34 +0300

    Example of a switch statements

--- js_tutorials/js7/js/switch.js
+++ js_tutorials/js7/js/switch.js
@@ -1,9 +1,7 @@
 // Conditional Statements: Switch statements
 
-const { Children } = require("react");
-
 // Syntax
-switch (expression OR value) {
+/*switch (expression OR value) {
     case choice1:
         // run this code
         break;
@@ -16,4 +14,16 @@ switch (expression OR value) {
     default:
         // run this code if no case matches
         // no need for break here
+}*/
+
+// Example:
+switch (Math.floor(Math.random() * 3) + 1) {
+  case 1:
+    console.log(1);
+  case 2:
+    console.log(2);
+  case 3:
+    console.log(3);
+  default:
+    console.log("No match");
 }
